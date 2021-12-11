415c9c8717f075503e5beb509bf5b15e0dd40769
Robert Chrzanowski <robert.chrzanowski@gmail.com>
2021-12-10 22:02:20 -0500

    Day 8, 9.

--- src/Day08.kt
+++ src/Day08.kt
@@ -1,18 +1,44 @@
 fun main() {
-    fun part1(input: List<String>): Int {
-        TODO()
+    fun part1(input: List<String>, rows: Int, cols: Int): Int {
+        val layers = input.joinToString("").map { it }.windowed(rows * cols, step = rows * cols)
+        val layersWithLeastZeros = layers.minByOrNull { layer -> layer.count { it == '0' } }!!
+        return layersWithLeastZeros.count { it == '1' } * layersWithLeastZeros.count { it == '2' }
     }
 
-    fun part2(input: List<String>): Int {
-        TODO()
+    fun part2(input: List<String>, rows: Int, cols: Int) {
+        input
+            .joinToString("").map { it }.windowed(rows * cols, step = rows * cols)
+            .reduceRight { a, b ->
+                b.indices.map { index ->
+                    when (a[index]) {
+                        '0' -> {
+                            a[index]
+                        }
+                        '1' -> {
+                            a[index]
+                        }
+                        '2' -> {
+                            b[index]
+                        }
+                        else -> {
+                            throw Exception()
+                        }
+                    }
+                }
+            }
+            .windowed(cols, cols)
+            .forEach { row ->
+                println(
+                    row.map { if (it == '0') ' ' else it }
+                        .joinToString("")
+                )
+            }
     }
 
     // test if implementation meets criteria from the description, like:
-    val testInput = readInput("Day08_test")
-    check(part1(testInput) == TODO())
-    check(part2(testInput) == TODO())
+    check(part1(listOf("123456789012"), 3, 2) == 1)
 
-    val input = readInput("Day08")
-    println(part1(input))
-    println(part2(input))
+    val input = readInput("Day09")
+    println(part1(input, 6, 25))
+    part2(input, 6, 25)
 }
--- src/Day09.kt
+++ src/Day09.kt
@@ -1,45 +1,112 @@
 fun main() {
-    fun part1(input: List<String>, rows: Int, cols: Int): Int {
-        val layers = input.joinToString("").map { it }.windowed(rows * cols, step = rows * cols)
-        val layersWithLeastZeros = layers.minByOrNull { layer -> layer.count { it == '0' } }!!
-        return layersWithLeastZeros.count { it == '1' } * layersWithLeastZeros.count { it == '2' }
-    }
+    fun part1(input: List<String>, inputValue: Long): List<Long> {
+        val opcodes: MutableMap<Long, Long> = input[0].split(",")
+            .mapIndexed { index, string -> Pair(index.toLong(), string.toLong()) }
+            .associate { it }
+            .toMutableMap()
 
-    fun part2(input: List<String>, rows: Int, cols: Int) {
-        input
-            .joinToString("").map { it }.windowed(rows * cols, step = rows * cols)
-            .reduceRight { a, b ->
-                b.indices.map { index ->
-                    when (a[index]) {
-                        '0' -> {
-                            a[index]
+        val output = mutableListOf<Long>()
+
+        var i = 0L
+        var rel = 0L
+
+        fun getIndex(modes: Long, index: Long): Long =
+            when (
+                (
+                        modes / when (index) {
+                            1L -> 1
+                            2L -> 10
+                            3L -> 100
+                            else -> throw Exception()
                         }
-                        '1' -> {
-                            a[index]
-                        }
-                        '2' -> {
-                            b[index]
-                        }
-                        else -> {
-                            throw Exception()
-                        }
+                        ) % 10
+            ) {
+                0L -> opcodes[opcodes[i + index] ?: 0] ?: 0
+                1L -> opcodes[i + index] ?: 0
+                2L -> opcodes[(opcodes[i + index] ?: 0) + rel] ?: 0
+                else -> throw Exception()
+            }
+
+        fun setIndex(modes: Long, index: Long, value: Long) {
+            opcodes[
+                    (opcodes[i + index] ?: 0) + when (
+                        (modes / when (index) {
+                            1L -> 1
+                            2L -> 10
+                            3L -> 100
+                            else -> throw Exception()
+                        }) % 10
+                    ) {
+                        0L -> 0
+                        2L -> rel
+                        else -> throw Exception()
+                    }
+            ] = value
+        }
+
+        while (true) {
+            val opcode = opcodes[i] ?: 0
+            val op = opcode % 100
+            val modes = opcode / 100
+
+            when (op) {
+                1L -> {
+                    setIndex(modes, 3, getIndex(modes, 1) + getIndex(modes, 2))
+                    i += 4
+                }
+                2L -> {
+                    setIndex(modes, 3, getIndex(modes, 1) * getIndex(modes, 2))
+                    i += 4
+                }
+                3L -> {
+                    setIndex(modes, 1, inputValue)
+                    i += 2
+                }
+                4L -> {
+                    output.add(getIndex(modes, 1))
+                    i += 2
+                }
+                5L -> {
+                    if (getIndex(modes, 1) != 0L) {
+                        i = getIndex(modes, 2)
+                    } else {
+                        i += 3
                     }
                 }
+                6L -> {
+                    if (getIndex(modes, 1) == 0L) {
+                        i = getIndex(modes, 2)
+                    } else {
+                        i += 3
+                    }
+                }
+                7L -> {
+                    setIndex(modes, 3, if (getIndex(modes, 1) < getIndex(modes, 2)) 1 else 0)
+                    i += 4
+                }
+                8L -> {
+                    setIndex(modes, 3, if (getIndex(modes, 1) == getIndex(modes, 2)) 1 else 0)
+                    i += 4
+                }
+                9L -> {
+                    rel += getIndex(modes, 1)
+                    i += 2
+                }
+                99L -> return output
+                else -> throw Exception("invalid op $i")
             }
-            .windowed(cols, cols)
-            .forEach { row ->
-                println(
-                    row.map { if (it == '0') ' ' else it }
-                        .joinToString("")
-                )
-            }
+        }
     }
 
     // test if implementation meets criteria from the description, like:
-//    val testInput = readInput("Day09_test")
-    check(part1(listOf("123456789012"), 3, 2) == 1)
+    check(
+        part1(listOf("109,1,204,-1,1001,100,1,100,1008,100,16,101,1006,101,0,99"), 1) ==
+                listOf(109L, 1, 204, -1, 1001, 100, 1, 100, 1008, 100, 16, 101, 1006, 101, 0, 99)
+    )
+    check(part1(listOf("1102,34915192,34915192,7,4,7,99,0"), 1) == listOf(1_219_070_632_396_864L))
+    check(part1(listOf("104,1125899906842624,99"), 1) == listOf(1_125_899_906_842_624L))
 
     val input = readInput("Day09")
-    println(part1(input, 6, 25))
-    part2(input, 6, 25)
+    println(part1(input, 1))
+    println(part1(input, 2))
 }
