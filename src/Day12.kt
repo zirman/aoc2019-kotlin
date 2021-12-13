92515fc91fd8398d9ac135c2bc0ea7d3726b5ff9
Robert Chrzanowski <robert.chrzanowski@gmail.com>
2021-12-12 19:14:57 -0500

    Day 12, 13.

--- src/Day12.kt
+++ src/Day12.kt
@@ -1,18 +1,125 @@
+import kotlin.math.absoluteValue
+import kotlin.math.max
+
+data class Vec3(val x: Int, val y: Int, val z: Int)
+
+operator fun Vec3.plus(v: Vec3): Vec3 =
+    Vec3(x + v.x, y + v.y, z + v.z)
+
+data class Moon(var position: Vec3, var velocity: Vec3)
+
 fun main() {
-    fun part1(input: List<String>): Int {
-        TODO()
+    fun part1(input: List<String>, iterations: Int): Long {
+        val moons = input.map { line ->
+            val groupValues = Regex("""<x=(-?\d+), y=(-?\d+), z=(-?\d+)>""").matchEntire(line)!!.groupValues
+            Moon(
+                Vec3(
+                    groupValues[1].toInt(),
+                    groupValues[2].toInt(),
+                    groupValues[3].toInt()
+                ),
+                Vec3(0, 0, 0)
+            )
+        }
+
+        for (i in 1..iterations) {
+            moons.forEach { moon ->
+                val (position, velocity) = moon
+
+                moon.velocity = moons.fold(velocity) { velocityAcc, otherMoon ->
+                    if (moon != otherMoon) {
+                        val other = otherMoon.position
+                        Vec3(
+                            x = velocityAcc.x + if (other.x > position.x) 1 else if (other.x < position.x) -1 else 0,
+                            y = velocityAcc.y + if (other.y > position.y) 1 else if (other.y < position.y) -1 else 0,
+                            z = velocityAcc.z + if (other.z > position.z) 1 else if (other.z < position.z) -1 else 0
+                        )
+                    } else {
+                        velocityAcc
+                    }
+                }
+            }
+
+            moons.forEach { moon ->
+                moon.position = moon.position + moon.velocity
+            }
+        }
+
+        return moons.fold(0L) { energy, (position, velocity) ->
+            energy + (position.x.absoluteValue + position.y.absoluteValue + position.z.absoluteValue) *
+                    (velocity.x.absoluteValue + velocity.y.absoluteValue + velocity.z.absoluteValue)
+        }
     }
 
-    fun part2(input: List<String>): Int {
-        TODO()
+    fun part2(input: List<String>): Long {
+        fun iterate(positions: IntArray, velocities: IntArray) {
+            positions.indices.forEach { i ->
+                (i + 1 until positions.size).forEach { k ->
+                    if (positions[i] < positions[k]) {
+                        velocities[i]++
+                        velocities[k]--
+                    } else if (positions[i] > positions[k]) {
+                        velocities[i]--
+                        velocities[k]++
+                    }
+                }
+            }
+
+            positions.indices.forEach { index -> positions[index] += velocities[index] }
+        }
+
+        fun cycles(p: List<Int>): Long {
+            val initialPositions = p.toIntArray()
+            val initialVelocities = p.map { 0 }.toIntArray()
+            val positions = initialPositions.copyOf()
+            val velocities = initialVelocities.copyOf()
+
+            for (i in 1..Long.MAX_VALUE) {
+                iterate(positions, velocities)
+
+                if (positions.contentEquals(initialPositions) &&
+                    velocities.contentEquals(initialVelocities)
+                ) {
+                    return i
+                }
+            }
+
+            throw Exception()
+        }
+
+        val positions = input.map { line ->
+            val groupValues = Regex("""<x=(-?\d+), y=(-?\d+), z=(-?\d+)>""").matchEntire(line)!!.groupValues
+            Vec3(
+                groupValues[1].toInt(),
+                groupValues[2].toInt(),
+                groupValues[3].toInt()
+            )
+        }
+
+        val xCycles = cycles(positions.map { (x, _, _) -> x })
+        val yCycles = cycles(positions.map { (_, y, _) -> y })
+        val zCycles = cycles(positions.map { (_, _, z) -> z })
+
+        val max = max(max(xCycles, yCycles), zCycles)
+
+        for (i in max until Long.MAX_VALUE step max) {
+            if (i % xCycles == 0L && i % yCycles == 0L && i % zCycles == 0L) {
+                return i
+            }
+        }
+
+        throw Exception()
     }
 
     // test if implementation meets criteria from the description, like:
     val testInput = readInput("Day12_test")
-    check(part1(testInput) == TODO())
-    check(part2(testInput) == TODO())
+    val testInput2 = readInput("Day12_test2")
+    check(part1(testInput, 10) == 179L)
+    check(part1(testInput2, 100) == 1_940L)
+    check(part2(testInput) == 2_772L)
+    check(part2(testInput2) == 4_686_774_924L)
 
     val input = readInput("Day12")
-    println(part1(input))
+    println(part1(input, 1000))
     println(part2(input))
 }
--- src/Day13.kt
+++ src/Day13.kt
@@ -1,16 +1,327 @@
+enum class PongMode {
+    ReadingX,
+    ReadingY,
+    ReadingTile,
+}
+
 fun main() {
     fun part1(input: List<String>): Int {
-        TODO()
+        fun machine(input: List<String>, read: () -> Long, write: (Long) -> Unit) {
+            val opcodes: MutableMap<Long, Long> = input[0].split(",")
+                .mapIndexed { index, string -> Pair(index.toLong(), string.toLong()) }
+                .associate { it }
+                .toMutableMap()
+
+            var i = 0L
+            var rel = 0L
+
+            fun getIndex(modes: Long, index: Long): Long =
+                when (
+                    (
+                            modes / when (index) {
+                                1L -> 1
+                                2L -> 10
+                                3L -> 100
+                                else -> throw Exception()
+                            }
+                            ) % 10
+                ) {
+                    0L -> opcodes[opcodes[i + index] ?: 0] ?: 0
+                    1L -> opcodes[i + index] ?: 0
+                    2L -> opcodes[(opcodes[i + index] ?: 0) + rel] ?: 0
+                    else -> throw Exception()
+                }
+
+            fun setIndex(modes: Long, index: Long, value: Long) {
+                opcodes[
+                        (opcodes[i + index] ?: 0) + when (
+                            (modes / when (index) {
+                                1L -> 1
+                                2L -> 10
+                                3L -> 100
+                                else -> throw Exception()
+                            }) % 10
+                        ) {
+                            0L -> 0
+                            2L -> rel
+                            else -> throw Exception()
+                        }
+                ] = value
+            }
+
+            while (true) {
+                val opcode = opcodes[i] ?: 0
+                val op = opcode % 100
+                val modes = opcode / 100
+
+                when (op) {
+                    1L -> {
+                        setIndex(modes, 3, getIndex(modes, 1) + getIndex(modes, 2))
+                        i += 4
+                    }
+                    2L -> {
+                        setIndex(modes, 3, getIndex(modes, 1) * getIndex(modes, 2))
+                        i += 4
+                    }
+                    3L -> {
+                        setIndex(modes, 1, read())
+                        i += 2
+                    }
+                    4L -> {
+                        write(getIndex(modes, 1))
+                        i += 2
+                    }
+                    5L -> {
+                        if (getIndex(modes, 1) != 0L) {
+                            i = getIndex(modes, 2)
+                        } else {
+                            i += 3
+                        }
+                    }
+                    6L -> {
+                        if (getIndex(modes, 1) == 0L) {
+                            i = getIndex(modes, 2)
+                        } else {
+                            i += 3
+                        }
+                    }
+                    7L -> {
+                        setIndex(modes, 3, if (getIndex(modes, 1) < getIndex(modes, 2)) 1 else 0)
+                        i += 4
+                    }
+                    8L -> {
+                        setIndex(modes, 3, if (getIndex(modes, 1) == getIndex(modes, 2)) 1 else 0)
+                        i += 4
+                    }
+                    9L -> {
+                        rel += getIndex(modes, 1)
+                        i += 2
+                    }
+                    99L -> return
+                    else -> throw Exception("invalid op $i")
+                }
+            }
+        }
+
+        var mode = PongMode.ReadingX
+        var x = 0L
+        var y = 0L
+
+        val tiles = mutableMapOf<Pair<Long, Long>, Long>()
+
+        machine(
+            input = input,
+            read = {
+                throw Exception()
+            },
+            write = { value ->
+                mode = when (mode) {
+                    PongMode.ReadingX -> {
+                        x = value
+                        PongMode.ReadingY
+                    }
+                    PongMode.ReadingY -> {
+                        y = value
+                        PongMode.ReadingTile
+                    }
+                    PongMode.ReadingTile -> {
+                        tiles[Pair(x, y)] = value
+                        PongMode.ReadingX
+                    }
+                }
+            }
+        )
+
+        return tiles.values.count { it == 2L }
     }
 
-    fun part2(input: List<String>): Int {
-        TODO()
+    fun part2(input: List<String>): Long {
+        fun machine(input: List<String>, read: () -> Long, write: (Long) -> Unit) {
+            val opcodes: MutableMap<Long, Long> = input[0].split(",")
+                .mapIndexed { index, string -> Pair(index.toLong(), string.toLong()) }
+                .associate { it }
+                .toMutableMap()
+
+            opcodes[0] = 2
+
+            var i = 0L
+            var rel = 0L
+
+            fun getIndex(modes: Long, index: Long): Long =
+                when (
+                    (
+                            modes / when (index) {
+                                1L -> 1
+                                2L -> 10
+                                3L -> 100
+                                else -> throw Exception()
+                            }
+                            ) % 10
+                ) {
+                    0L -> opcodes[opcodes[i + index] ?: 0] ?: 0
+                    1L -> opcodes[i + index] ?: 0
+                    2L -> opcodes[(opcodes[i + index] ?: 0) + rel] ?: 0
+                    else -> throw Exception()
+                }
+
+            fun setIndex(modes: Long, index: Long, value: Long) {
+                opcodes[
+                        (opcodes[i + index] ?: 0) + when (
+                            (modes / when (index) {
+                                1L -> 1
+                                2L -> 10
+                                3L -> 100
+                                else -> throw Exception()
+                            }) % 10
+                        ) {
+                            0L -> 0
+                            2L -> rel
+                            else -> throw Exception()
+                        }
+                ] = value
+            }
+
+            while (true) {
+                val opcode = opcodes[i] ?: 0
+                val op = opcode % 100
+                val modes = opcode / 100
+
+                when (op) {
+                    1L -> {
+                        setIndex(modes, 3, getIndex(modes, 1) + getIndex(modes, 2))
+                        i += 4
+                    }
+                    2L -> {
+                        setIndex(modes, 3, getIndex(modes, 1) * getIndex(modes, 2))
+                        i += 4
+                    }
+                    3L -> {
+                        setIndex(modes, 1, read())
+                        i += 2
+                    }
+                    4L -> {
+                        write(getIndex(modes, 1))
+                        i += 2
+                    }
+                    5L -> {
+                        if (getIndex(modes, 1) != 0L) {
+                            i = getIndex(modes, 2)
+                        } else {
+                            i += 3
+                        }
+                    }
+                    6L -> {
+                        if (getIndex(modes, 1) == 0L) {
+                            i = getIndex(modes, 2)
+                        } else {
+                            i += 3
+                        }
+                    }
+                    7L -> {
+                        setIndex(modes, 3, if (getIndex(modes, 1) < getIndex(modes, 2)) 1 else 0)
+                        i += 4
+                    }
+                    8L -> {
+                        setIndex(modes, 3, if (getIndex(modes, 1) == getIndex(modes, 2)) 1 else 0)
+                        i += 4
+                    }
+                    9L -> {
+                        rel += getIndex(modes, 1)
+                        i += 2
+                    }
+                    99L -> return
+                    else -> throw Exception("invalid op $i")
+                }
+            }
+        }
+
+        var mode = PongMode.ReadingX
+        var x = 0L
+        var y = 0L
+
+        val tiles = mutableMapOf<Pair<Long, Long>, Long>()
+        var score = 0L
+
+        machine(
+            input = input,
+            read = {
+//                (tiles.keys.map { (_, y) -> y }.minOf { it }..tiles.keys.map { (_, y) -> y }.maxOf { it })
+//                    .forEach { y ->
+//                        (tiles.keys.map { (x, _) -> x }.minOf { it }..tiles.keys.map { (x, _) -> x }.maxOf { it })
+//                            .forEach { x ->
+//                                print(
+//                                    when (tiles[Pair(x, y)]) {
+//                                        0L -> ' '
+//                                        1L -> '#'
+//                                        2L -> 'X'
+//                                        3L -> '-'
+//                                        4L -> '.'
+//                                        else -> ' '
+//                                    }
+//                                )
+//                            }
+//
+//                        println()
+//                    }
+
+                //                readln()
+
+                val (ballPosition, _) = tiles.asSequence().first { (_, v) -> v == 4L }
+                val (paddlePosition, _) = tiles.asSequence().first { (_, v) -> v == 3L }
+
+                if (ballPosition.first < paddlePosition.first) {
+                    -1
+                } else if (ballPosition.first > paddlePosition.first) {
+                    1
+                } else {
+                    0
+                }
+            },
+            write = { value ->
+                mode = when (mode) {
+                    PongMode.ReadingX -> {
+                        x = value
+                        PongMode.ReadingY
+                    }
+                    PongMode.ReadingY -> {
+                        y = value
+                        PongMode.ReadingTile
+                    }
+                    PongMode.ReadingTile -> {
+                        if (x == -1L && y == 0L) {
+                            score = value
+                        } else {
+                            tiles[Pair(x, y)] = value
+                        }
+
+                        PongMode.ReadingX
+                    }
+                }
+            }
+        )
+
+        return score
+//        (tiles.keys.map { (_, y) -> y }.minOf { it }..tiles.keys.map { (_, y) -> y }.maxOf { it })
+//            .forEach { y ->
+//                (tiles.keys.map { (x, _) -> x }.minOf { it }..tiles.keys.map { (x, _) -> x }.maxOf { it })
+//                    .forEach { x ->
+//                        print(
+//                            when (tiles[Pair(x, y)]) {
+//                                0L -> ' '
+//                                1L -> '#'
+//                                2L -> 'X'
+//                                3L -> '-'
+//                                4L -> '.'
+//                                else -> ' '
+//                            }
+//                        )
+//                    }
+//
+//                println()
+//            }
     }
 
     // test if implementation meets criteria from the description, like:
-    val testInput = readInput("Day13_test")
-    check(part1(testInput) == TODO())
-    check(part2(testInput) == TODO())
 
     val input = readInput("Day13")
     println(part1(input))
